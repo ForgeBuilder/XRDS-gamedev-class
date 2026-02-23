7efef395b85b4e2daa7a34e8cb89d9bdd464c2d6
ForgeBuilder <elliotkarlson@gmail.com>
2026-02-23 09:51:05 -0600

    changes to launching allignment code
    a big problem with the new motor configuration.. it's causing mad vibrations that are messing with the camera.

--- player_car.gd
+++ player_car.gd
@@ -1,17 +1,21 @@
 extends RigidBody2D
 
 const SPEED = 200
-const TURN_SPEED = 5000
-const drift_factor = 0.2
+const TURN_SPEED = 8000
+const drift_factor = 0.8
 const brake_factor = 0.95
 
 @onready var diolouge_text_box = $Diolouge_text
+
+var turn_input : float
 
 func _physics_process(delta: float) -> void:
 	
 	apply_central_force(Vector2.UP.rotated(self.rotation)*SPEED*Input.get_axis("ui_down","ui_up"))
 	
-	apply_torque(Input.get_axis("ui_left","ui_right")*TURN_SPEED)
+	turn_input = Input.get_axis("ui_left","ui_right")*0.01*sqrt(linear_velocity.length())/4
+	
+	#apply_torque()
 	
 	
 
@@ -20,6 +24,7 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var right_velocity = transform.x * state.linear_velocity.dot(transform.x)
 	
 	state.linear_velocity = forward_velocity + right_velocity*drift_factor
+	state.transform = state.transform.rotated_local(turn_input)
 	
 	if Input.is_action_pressed("ui_accept"):
 		state.linear_velocity = state.linear_velocity*brake_factor
